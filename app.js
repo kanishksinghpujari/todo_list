a347fe802039a83fecdc208a13f07cd84241d8dc
kanishksinghpujari <68298682+kanishksinghpujari@users.noreply.github.com>
2021-06-05 10:06:41 +0530

    final

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const date=require(__dirname+"/function.js");
-var items=[];
+
 const app=express();
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static("public"));
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model('Item', itemsSchema);
+
 app.get("/",function(req,res){
 //
 const day=date.getDay();
+Item.find({},function(err,foundItems){
   res .render("list",{
     day:day,
-    items:items
+    items:foundItems
 
   });
 });
 
+});
+
 app.post("/",function(req,res){
-  const x=req.body.next;
-  items.push(x);
-res.redirect("/");
+  const x={
+    name:req.body.next};
+  Item.insertMany([x], function(error) {
+    if(error)
+    console.log("gadbad");
+    else
+    res.redirect("/");
+  });
+
+
 });
 
 app.listen(3000,function(){
